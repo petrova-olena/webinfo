efeeabfb532018ed65db371ac2f29d56ab18e3a9
petrova-olena <petrova.olena.anatoliivna@gmail.com>
2025-05-13 12:36:21 +0300

    Initial commit: Vite + React project

---
+++ src/App.jsx
@@ -0,0 +1,126 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>HTML documentation</h1>
+      <h3>1. Main (root)</h3>
+      <p>html</p>
+      <p>head</p>
+      <p>body</p>
+      <h3>2. Metadata and management</h3>
+      <p>base</p>
+      <p>link</p>
+      <p>meta</p>
+      <p>style</p>
+      <p>title</p>
+      <h3>3. Document section</h3>
+      <p>header</p>
+      <p>nav</p>
+      <p>main</p>
+      <p>section</p>
+      <p>article</p>
+      <p>aside</p>
+      <p>footer</p>
+      <p>address</p>
+      <h3>4. Textual and semantic</h3>
+      <p>h1 - h6</p>
+      <p>p</p>
+      <p>hr</p>
+      <p>pre</p>
+      <p>blockquote</p>
+      <p>ol</p>
+      <p>ul</p>
+      <p>li</p>
+      <p>dl</p>
+      <p>dt</p>
+      <p>dd</p>
+      <p>figure</p>
+      <p>figcaption</p>
+      <p>div</p>
+      <h3>5. Inline text</h3>
+      <p>a</p>
+      <p>abbr</p>
+      <p>b</p>
+      <p>bdi</p>
+      <p>br</p>
+      <p>cite</p>
+      <p>code</p>
+      <p>data</p>
+      <p>dfn</p>
+      <p>em</p>
+      <p>i</p>
+      <p>kbd</p>
+      <p>mark</p>
+      <p>q</p>
+      <p>rp</p>
+      <p>rt</p>
+      <p>ruby</p>
+      <p>s</p>
+      <p>samp</p>
+      <p>small</p>
+      <p>span</p>
+      <p>strong</p>
+      <p>sub</p>
+      <p>sup</p>
+      <p>time</p>
+      <p>u</p>
+      <p>var</p>
+      <p>wbr</p>
+      <h3>6. Media</h3>
+      <p>img</p>
+      <p>audio</p>
+      <p>video</p>
+      <p>source</p>
+      <p>track</p>
+      <p>map</p>
+      <p>area</p>
+      <p>picture</p>
+      <p>canvas</p>
+      <p>svg (SVG graphics are embedded)</p>
+      <h3>7. Forms</h3>
+      <p>form</p>
+      <p>input</p>
+      <p>textarea</p>
+      <p>button</p>
+      <p>select</p>
+      <p>option</p>
+      <p>optgroup</p>
+      <p>label</p>
+      <p>fieldset</p>
+      <p>legend</p>
+      <p>datalist</p>
+      <p>output</p>
+      <p>meter</p>
+      <p>progress</p>
+      <h3>8. Tables</h3>
+      <p>table</p>
+      <p>caption</p>
+      <p>thead</p>
+      <p>tbody</p>
+      <p>tfoot</p>
+      <p>tr</p>
+      <p>th</p>
+      <p>td</p>
+      <h3>9. Scenarios and interaction</h3>
+      <p>script</p>
+      <p>noscript</p>
+      <p>template</p>
+      <p>slot (in Web Components)</p>
+      <p>canvas</p>
+      <p>dialog</p>
+      <p>details</p>
+      <p>summary</p>
+      <h3>10. Framing and embedding</h3>
+      <p>iframe</p>
+      <p>embed</p>
+      <p>object</p>
+      <p>param</p>
+    </>
+  )
+}
+
+export default App
